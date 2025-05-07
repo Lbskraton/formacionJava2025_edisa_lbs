a09fe0925b5b07cd98d5305037a8d2136096a203
Ismael Álvarez Iglesias <ismael.alvarez@edisa.com>
2025-05-07 18:28:58 +0200

    Ejemplo DropWizard

---
+++ src/main/java/com/edisa/formacion/mayo2025/DropWizardConfiguration.java
@@ -0,0 +1,5 @@
+package com.edisa.formacion.mayo2025;
+import io.dropwizard.Configuration;
+
+public class DropWizardConfiguration extends Configuration {
+}
--- src/main/java/com/edisa/formacion/mayo2025/Main.java
+++ src/main/java/com/edisa/formacion/mayo2025/Main.java
@@ -1,8 +1,26 @@
 package com.edisa.formacion.mayo2025;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Numero de argumentos"+String.valueOf(args.length));
-        System.out.println("Hello world! ");
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+
+public class Main extends Application<DropWizardConfiguration> {
+    public static void main(String[] args) throws Exception {
+        new Main().run(args);
+
+    }
+    @Override
+    public void initialize(Bootstrap<DropWizardConfiguration> bootstrap) {
+        // Configuración adicional si es necesaria
+    }
+
+    @Override
+    public void run(DropWizardConfiguration configuration, Environment environment) {
+        final Recursos resource = new Recursos();
+        environment.jersey().register(resource);
     }
 }
